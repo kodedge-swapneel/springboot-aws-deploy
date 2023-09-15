56f37820afef1f0795af4f46a9976e6cd356082e
swapneelyashwantgosavi <swap24gosavi@gmail.com>
2023-09-15 18:43:18 +0530

    first commit

---
+++ src/main/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootawsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootawsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
