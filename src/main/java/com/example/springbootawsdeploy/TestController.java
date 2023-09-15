de6ddf8a378b62e173874cfd2165596665129603
swapneelyashwantgosavi <swap24gosavi@gmail.com>
2023-09-15 20:13:53 +0530

    new api

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -11,4 +11,9 @@ public class TestController {
     public String getData() {
         return "First message from AWS Ecs";
     }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return "Second message from AWS Ecs";
+    }
 }
